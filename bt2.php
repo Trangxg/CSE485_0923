819bb2308f924a78565fed14f6734ff137e14263
Trangxg <92989660+Trangxg@users.noreply.github.com>
2023-09-19 08:51:23 +0700

    Add files via upload

---
+++ bt2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>bai2</title>
+</head>
+<body>
+  <?php
+  $arrs = array('đỏ','xanh','cam','trắng');
+  $alert=  "Màu đỏ là màu yêu thích của Anh, trắng là màu yêu thích của Sơn, cam là màu yêu thích của Thắng, còn màu yêu thích của tôi là màu trắng";
+
+  echo $alert;
+
+
+
+// Mảng chứa các giá trị
+
+
+  ?>
+</body>
+</html>
